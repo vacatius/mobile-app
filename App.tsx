5a67e95b898def95c2c4615d417a01d34bcb38d9
whiteclouds7 <65771894+whiteclouds7@users.noreply.github.com>
2021-04-10 12:02:26 +0200

    add react-native-elements

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Icon} from "react-native-elements";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World</Text>
+      <Icon
+          reverse
+          name='beer'
+          type='font-awesome-5'
+          color='#f50'
+          onPress={() => console.log('hello')} />
       <StatusBar style="auto" />
     </View>
   );
