14fa4cccc306954e2cd762ce5362e252c72e65b1
valentingc <26229513+valentingc@users.noreply.github.com>
2021-04-13 10:44:41 +0200

    show trip dashboard per default (temporarily)

--- src/screens/TripsDashboard.tsx
+++ src/screens/TripsDashboard.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { RootStackParamList } from "../../App";
 import ScreenHeader from "../components/ScreenHeader";
+import { StackNavigationProp } from "@react-navigation/stack";
 
-export default function Register() {
+type TripsDashboardScreenNavigationProp = StackNavigationProp<
+    RootStackParamList,
+    "Dashboard"
+>;
+
+type Props = {
+    navigation: TripsDashboardScreenNavigationProp;
+};
+
+export default function TripsDashboard(props: Props) {
     const { t } = useTranslation();
-    return (
-        <ScreenHeader screenTitle={t("screen_header_trip_dashBoard")} />
-    )
+    return <ScreenHeader screenTitle={t("screen_header_trip_dashBoard")} />;
 }
