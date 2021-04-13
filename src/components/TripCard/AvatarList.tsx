7438069db97afe1ca0912f5da5283755f4254843
valentingc <26229513+valentingc@users.noreply.github.com>
2021-04-13 20:21:56 +0200

    add avatar popover in tripcard

--- src/components/TripCard/AvatarList.tsx
+++ src/components/TripCard/AvatarList.tsx
@@ -1,28 +1,39 @@
 import React from "react";
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { Avatar } from "react-native-elements";
+import { Tooltip } from "react-native-elements/dist/tooltip/Tooltip";
 import { TripMember } from "../../types";
 import { TripsQuery } from "./types/trip-dashboard.query";
 
 export interface AvatarListProps {
-    // TODO
     tripMembers: TripsQuery["trips"][0]["members"];
 }
-
 
 const AvatarList: React.FC<AvatarListProps> = (props: AvatarListProps) => {
     return (
         <FlatList
             horizontal
-            data={props.tripMembers} // TODO
+            data={props.tripMembers}
             renderItem={({ item: member }) => {
                 return (
-                    <Avatar
-                        rounded
-                        containerStyle={{ backgroundColor: member.color }}
-                        size={"medium"}
-                        title={member.user.displayName.charAt(0)?.toUpperCase() ?? "?"}
-                    />
+                    <View>
+                        <Tooltip
+                            popover={<Text>{member.user.displayName}</Text>}
+                        >
+                            <Avatar
+                                rounded
+                                containerStyle={{
+                                    backgroundColor: member.color,
+                                }}
+                                size={"medium"}
+                                title={
+                                    member.user.displayName
+                                        .charAt(0)
+                                        ?.toUpperCase() ?? "?"
+                                }
+                            />
+                        </Tooltip>
+                    </View>
                 );
             }}
             keyExtractor={(member) => member.id}
