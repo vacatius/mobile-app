9a8634998c2948d6aea87f270b6937e491c4e87b
Alexander Weber <43758870+alexw-at@users.noreply.github.com>
2021-04-15 14:39:26 +0200

    add formik to login screen

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -5,10 +5,12 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import SvgLogo from "../../components/SvgLogo";
 import { useTranslation } from "react-i18next";
 import * as SecureStore from "expo-secure-store";
-
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../../../App";
 import { useLoginMutation } from "./types/loginMutation";
+import { Formik, FormikValues } from "formik";
+import * as Yup from "yup";
+import { TFunction } from "i18next";
 
 type ProfileScreenNavigationProp = StackNavigationProp<
     RootStackParamList,
@@ -25,10 +27,6 @@ async function saveInSecureStore(key: string, value: string) {
 
 export default function Login(props: Props) {
     const { t } = useTranslation();
-    const [username, setUsername] = useState("");
-    const [usernameError, setUsernameError] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordError, setPasswordError] = useState("");
     const [placeholder, setPlaceholder] = useState({
         username: t("placeholder.username", { returnObjects: true })[
             Math.floor(
@@ -49,19 +47,11 @@ export default function Login(props: Props) {
     });
     const [execute, { error, loading }] = useLoginMutation();
 
-    const handleLogin = () => {
-        if (username === "") {
-            setUsernameError(t("error.username"));
-        }
-        if (password === "") {
-            setPasswordError(t("error.password"));
-        }
-        if (password === "" || username === "") {
-            return;
-        }
-
+    const handleLogin = (values: FormikValues) => {
         execute({
-            variables: { input: { password: password, username: username } },
+            variables: {
+                input: { password: values.password, username: values.username },
+            },
         })
             .then((res) => {
                 if (res.data?.login.token) {
@@ -81,78 +71,97 @@ export default function Login(props: Props) {
             <SafeAreaView style={styles.container}>
                 <SvgLogo style={styles.logo} width={150} height={150} />
                 <Text style={styles.text}>{t("login")}</Text>
-                <Input
-                    label={t("username")}
-                    errorMessage={usernameError}
-                    errorStyle={styles.errorMessage}
-                    placeholder={placeholder.username}
-                    leftIcon={
-                        <Icon
-                            style={styles.icon}
-                            name="user"
-                            size={24}
-                            color="black"
-                            type="font-awesome-5"
-                        />
-                    }
-                    value={username}
-                    onChange={(e) => {
-                        setUsername(e.nativeEvent.text);
-                        if (usernameError) {
-                            setUsernameError("");
-                        }
-                    }}
-                />
-                <Input
-                    label={t("password")}
-                    errorMessage={passwordError}
-                    errorStyle={styles.errorMessage}
-                    placeholder={placeholder.password}
-                    leftIcon={
-                        <Icon
-                            style={styles.icon}
-                            name="lock"
-                            size={24}
-                            color="black"
-                            type="font-awesome-5"
-                        />
-                    }
-                    value={password}
-                    secureTextEntry={true}
-                    onChange={(e) => {
-                        setPassword(e.nativeEvent.text);
-                        if (passwordError) {
-                            setPasswordError("");
-                        }
-                    }}
-                />
-                {error && (
-                    <Text style={styles.errorText}>
-                        {error.message.includes("credentials")
-                            ? t("error.credentials")
-                            : error.networkError
-                            ? t("error.network")
-                            : error.message}
-                    </Text>
-                )}
-                <Button
-                    containerStyle={styles.buttonContainer}
-                    buttonStyle={styles.buttonLogin}
-                    title={t("login")}
-                    titleStyle={{ color: "black", fontSize: 25 }}
-                    icon={
-                        <Icon
-                            style={styles.iconButton}
-                            name="arrow-right"
-                            size={15}
-                            color="black"
-                            type="font-awesome-5"
-                        />
-                    }
-                    iconRight={true}
-                    onPress={handleLogin}
-                    loading={loading}
-                />
+                <Formik
+                    initialValues={{ username: "", password: "" }}
+                    onSubmit={handleLogin}
+                    validationSchema={validationSchema(t)}
+                >
+                    {({
+                        handleChange,
+                        values,
+                        handleSubmit,
+                        errors,
+                        isValid,
+                        isSubmitting,
+                        touched,
+                        handleBlur,
+                    }) => (
+                        <>
+                            <Input
+                                label={t("username")}
+                                errorMessage={
+                                    errors.username && touched.username
+                                        ? errors.username
+                                        : undefined
+                                }
+                                errorStyle={styles.errorMessage}
+                                placeholder={placeholder.username}
+                                leftIcon={
+                                    <Icon
+                                        style={styles.icon}
+                                        name="user"
+                                        size={24}
+                                        color="black"
+                                        type="font-awesome-5"
+                                    />
+                                }
+                                value={values.username}
+                                onChangeText={handleChange("username")}
+                                onBlur={handleBlur("username")}
+                            />
+                            <Input
+                                label={t("password")}
+                                errorMessage={
+                                    errors.password && touched.password
+                                        ? errors.password
+                                        : undefined
+                                }
+                                errorStyle={styles.errorMessage}
+                                placeholder={placeholder.password}
+                                leftIcon={
+                                    <Icon
+                                        style={styles.icon}
+                                        name="lock"
+                                        size={24}
+                                        color="black"
+                                        type="font-awesome-5"
+                                    />
+                                }
+                                value={values.password}
+                                secureTextEntry={true}
+                                onChangeText={handleChange("password")}
+                                onBlur={handleBlur("password")}
+                            />
+                            {error && (
+                                <Text style={styles.errorText}>
+                                    {error.message.includes("credentials")
+                                        ? t("error.credentials")
+                                        : error.networkError
+                                        ? t("error.network")
+                                        : error.message}
+                                </Text>
+                            )}
+                            <Button
+                                containerStyle={styles.buttonContainer}
+                                buttonStyle={styles.buttonLogin}
+                                title={t("login")}
+                                titleStyle={{ color: "black", fontSize: 25 }}
+                                icon={
+                                    <Icon
+                                        style={styles.iconButton}
+                                        name="arrow-right"
+                                        size={15}
+                                        color="black"
+                                        type="font-awesome-5"
+                                    />
+                                }
+                                iconRight={true}
+                                onPress={() => handleSubmit()}
+                                loading={loading}
+                            />
+                        </>
+                    )}
+                </Formik>
                 <Button
                     containerStyle={styles.buttonContainer}
                     type="clear"
@@ -164,6 +173,17 @@ export default function Login(props: Props) {
         </ScrollView>
     );
 }
+
+const validationSchema = (t: TFunction): object => {
+    return Yup.object().shape({
+        username: Yup.string()
+            .min(1)
+            .required(t("validation.usernameRequired")),
+        password: Yup.string()
+            .min(1)
+            .required(t("validation.password.required")),
+    });
+};
 
 const styles = StyleSheet.create({
     container: {
