3782246020bab7b7b77c5d6b9b564f6d0e1e3340
valentingc <26229513+valentingc@users.noreply.github.com>
2021-04-20 11:00:49 +0200

    remove console from bad merge

--- src/screens/TripsDashboard/TripsDashboard.tsx
+++ src/screens/TripsDashboard/TripsDashboard.tsx
@@ -36,7 +36,6 @@ export default function TripsDashboard(props: Props): JSX.Element {
     useEffect(() => {
         console.debug("[TripsDashboard] Trips data has changed");
         const currentTripsFiltered = tripsData?.trips.filter((trip) => {
-            console.log(trip)
             return (
                 (trip.startDate === null && trip.endDate === null) ||
                 new Date(trip.endDate).getTime() >= new Date().getTime()
